83e3a288b7a2f74326b69a9a4cc325463f8ac3ad
taha-riz <taharizwanmughal@gmail.com>
2023-06-05 22:16:08 +0500

    Basic Sanity Functionality added

---
+++ sanity/product.ts
@@ -0,0 +1,22 @@
+export const products = {
+  name: "product",
+  title: "Product",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Product Title",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Product Description",
+      type: "string",
+    },
+    {
+      name: "image",
+      title: "Product Image",
+      type: "string",
+    },
+  ],
+};
